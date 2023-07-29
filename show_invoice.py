9542593440206db4d0c97e4aeeb11018e2e3472b
mitsonagaoka <mmaro.mitsu825@gmail.com>
2023-07-29 14:59:49 +0900

    13th commit

--- show_invoice.py
+++ show_invoice.py
@@ -1,29 +1,25 @@
 # streamlit run show_invoice.py
 
+import os
 import sqlite3
+from datetime import date
+
 import pandas as pd
+import qrcode
 import streamlit as st
 from PIL import Image
-# import datetime
-from datetime import date
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import portrait, A4
-# from reportlab.lib.utils import ImageReader
+from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
-# from reportlab.pdfbase.cidfonts import UnicodeCIDFont
-# from contextlib import closing
-# import plotly.express as px
-# import base64
-import qrcode
-# from pdf2image import convert_from_path
 from reportlab.pdfbase.ttfonts import TTFont
-# import pytesseract
+from reportlab.pdfgen import canvas
 
-# import fitz  # PyMuPDFをインポート
-# import tempfile
-import os
 
-def show_invoice43():
+# PDFを作成する関数
+def create_pdf(file_path):
+    # Canvasオブジェクトを作成（A4サイズ）
+    cv = canvas.Canvas(file_path, pagesize=A4)
+
+    # ここにPDFの内容を追加するコードを記述
     # title表示する
     st.title('月単位請求項目表示4')
     st.subheader('月単位請求を抽出します。')
@@ -41,14 +37,14 @@ def show_invoice43():
     end_date = st.date_input("終了日を選択してください")
 
     if st.button('実行'):
-        global qd_会社id
+        # global qd_会社id
         # SQLクエリの作成と実行
         sql = """
-            select t_出荷Data.品番, t_在庫Data.名称, t_在庫Data.単価, t_出荷Data.出荷数, t_出荷Data.出荷日, t_在庫Data.Tax, t_出荷Data.出荷金額
-            FROM t_出荷Data INNER JOIN t_在庫Data
-            ON t_出荷Data.品番 = t_在庫Data.品番
-            WHERE t_出荷Data.出荷日 BETWEEN ? AND ?
-        """
+                select t_出荷Data.品番, t_在庫Data.名称, t_在庫Data.単価, t_出荷Data.出荷数, t_出荷Data.出荷日, t_在庫Data.Tax, t_出荷Data.出荷金額
+                FROM t_出荷Data INNER JOIN t_在庫Data
+                ON t_出荷Data.品番 = t_在庫Data.品番
+                WHERE t_出荷Data.出荷日 BETWEEN ? AND ?
+            """
         c.execute(sql, (start_date.strftime('%Y/%m/%d'), end_date.strftime('%Y/%m/%d')))
 
         # SELECT COUNT(*) FROM t_出荷Data
@@ -82,196 +78,167 @@ def show_invoice43():
         # データベースとの接続を閉じる
         conn.close()
 
-        def create_pdf(file_path):
 
-            ###### Invoice帳票 And QRコードを作成 ######################################################################
-            qr = qrcode.QRCode(
-                version=10,
-                error_correction=qrcode.constants.ERROR_CORRECT_H,
-                box_size=2,
-                border=8
-            )
+        ###### Invoice帳票 And QRコードを作成 ######################################################################
+        qr = qrcode.QRCode(
+            version=10,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=2,
+            border=8
+        )
 
-            # q_data = 'https://engineer-lifestyle-blog.com/'
-            q_data = 'df'
+        # q_data = 'https://engineer-lifestyle-blog.com/'
+        q_data = 'df'
 
-            qr.add_data(q_data)
-            qr.make()
-            _img = qr.make_image(fill_color="black", back_color="#ffffff")
-            _img.save('./data/qrcode45.png')
-            img = Image.open('./data/qrcode45.png')
-            img.show()
+        qr.add_data(q_data)
+        qr.make()
+        _img = qr.make_image(fill_color="black", back_color="#ffffff")
+        _img.save('./data/qrcode45.png')
+        img = Image.open('./data/qrcode45.png')
+        img.show()
 
-            ###### Invoice_issue を発行######################################################################
-            # MSGothic フォントの絶対パスを指定
-            font_file_path = 'C:\\Windows\\Fonts\\msgothic.ttc'  # または 'C:\\Windows\\Fonts\\msgothic.ttf'
+        ###### Invoice_issue を発行######################################################################
+        # MSGothic フォントの絶対パスを指定
+        font_file_path = 'C:\\Windows\\Fonts\\msgothic.ttc'  # または 'C:\\Windows\\Fonts\\msgothic.ttf'
 
-            # フォントを登録
-            pdfmetrics.registerFont(TTFont('MSGothic', font_file_path))
+        # フォントを登録
+        pdfmetrics.registerFont(TTFont('MSGothic', font_file_path))
 
-            # PDFを作成 # 日付をYYYYMMDD形式に変換し、ファイル名に使用する
-            today = date.today()
-            today_str_cnv = today.strftime('%Y%m%d')
+        # フォントサイズ定義
+        font_size1 = 20
+        font_size2 = 14
+        font_size3 = 10
+        cv.setFont('MSGothic', font_size2)
 
-            # st.write = today_str_cnv
+        # 表題欄 (x座標, y座標, 文字)を指定
+        cv.setFont('MSGothic', font_size1)
+        cv.drawString(50, 760, 'Invoice(期間別[月/日/任意]請求書)')
 
-            file_name = f'Invoice_{today_str_cnv}.pdf'
-            cv = canvas.Canvas(file_name, pagesize=portrait(A4))
+        cv.setFont('MSGothic', font_size2)
+        cv.drawString(450, 760, f'Inv- {today_str_cnv}')
 
-            # 保存先のディレクトリパス
-            output_directory = "C:\\Users\\Invoice"
+        # 画像挿入(画像パス、始点x、始点y、幅、高さ) watermark.jpg
+        cv.drawInlineImage('./data/com_logo01.png', 470, 670, 80, 80)
+        cv.drawInlineImage('./data/qrcode45.png', 40, 25, 100, 100)
 
-            # ファイルのフルパス
-            pdf_path = os.path.join(output_directory, file_name)
+        # 顧客Dataより、[会社名][Email]][TEL][担当者]を抽出する
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
 
-            # フォントサイズ定義
-            font_size1 = 20
-            font_size2 = 14
-            font_size3 = 10
-            cv.setFont('MSGothic', font_size2)
+        qd_会社id = '1'
+        Comid_sql = conn.execute('SELECT * FROM t_顧客Data WHERE 会社ID = ?', (qd_会社id,)).fetchall()
 
-            # 表題欄 (x座標, y座標, 文字)を指定
-            cv.setFont('MSGothic', font_size1)
-            cv.drawString(50, 760, 'Invoice(期間別[月/日/任意]請求書)')
+        # 顧客Dataの抽出
+        dd_会社名 = Comid_sql[0][1]
+        dd_Email = Comid_sql[0][2]
+        dd_TEL = Comid_sql[0][5]
+        dd_担当者 = Comid_sql[0][7]
 
-            cv.setFont('MSGothic', font_size2)
-            cv.drawString(450, 760, f'Inv- {today_str_cnv}')
+        conn.commit()
+        conn.close()
 
-            # 画像挿入(画像パス、始点x、始点y、幅、高さ) watermark.jpg
-            cv.drawInlineImage('./data/com_logo01.png', 470, 670, 80, 80)
-            cv.drawInlineImage('./data/qrcode45.png', 40, 25, 100, 100)
+        # 見出し表題を記入する
+        cv.drawString(50, 695, f'{dd_会社名} 御中　{dd_担当者}　様')
+        cv.drawString(50, 680, dd_Email)
+        cv.drawString(50, 665, dd_TEL)
+        cv.drawString(430, 655, '（株）仁志田製作所')
+        cv.drawString(405, 640, '東京都板橋区中台1-3-5')
+        cv.drawString(385, 625, 'Supplier5678@gmail.com')
+        cv.drawString(450, 610, '03(3111)1200')
+        # cv.setFont('HeiseiKakuGo-W5', font_size2)
+        cv.setFont('MSGothic', font_size2)
 
-            # 顧客Dataより、[会社名][Email]][TEL][担当者]を抽出する
-            conn = sqlite3.connect(db_name)
-            c = conn.cursor()
+        # 期間の表材の作成
+        string11 = f'{start_date} から{end_date}'
+        cv.drawString(100, 570, f'{string11}納入分 請求金額:￥{int(dd_請求金額):,}円')
 
-            qd_会社id = '1'
-            Comid_sql = conn.execute('SELECT * FROM t_顧客Data WHERE 会社ID = ?', (qd_会社id,)).fetchall()
+        cv.setLineWidth(1.5)
+        # 線を描画(始点x、始点y、終点x、終点y)
+        cv.line(95, 562, 520, 562)
+        cv.line(45, 755, 560, 755)
 
-            # 顧客Dataの抽出
-            dd_会社名 = Comid_sql[0][1]
-            dd_Email = Comid_sql[0][2]
-            dd_TEL = Comid_sql[0][5]
-            dd_担当者 = Comid_sql[0][7]
+        # cv.setFont('HeiseiKakuGo-W5', font_size3)
+        cv.setFont('MSGothic', font_size3)
 
-            conn.commit()
-            conn.close()
+        # ヘッター欄
+        cv.drawString(52, 534, '品  番')
+        cv.drawString(132, 534, '名  称')
+        cv.drawString(262, 534, '単  価')
+        cv.drawString(312, 534, '出荷数')
+        cv.drawString(367, 534, '出荷日')
+        cv.drawString(447, 534, 'Tax')
+        cv.drawString(492, 534, '出荷金額')
 
-            # 見出し表題を記入する
-            cv.drawString(50, 695, f'{dd_会社名} 御中　{dd_担当者}　様')
-            cv.drawString(50, 680, dd_Email)
-            cv.drawString(50, 665, dd_TEL)
-            cv.drawString(430, 655, '（株）仁志田製作所')
-            cv.drawString(405, 640, '東京都板橋区中台1-3-5')
-            cv.drawString(385, 625, 'Supplier5678@gmail.com')
-            cv.drawString(450, 610, '03(3111)1200')
-            # cv.setFont('HeiseiKakuGo-W5', font_size2)
-            cv.setFont('MSGothic', font_size2)
+        # 一列目[品番]
+        for i in range(len(df)):
+            cv.drawString(42, 514 - (i * 20), str(df.iloc[i, 0]))
 
-            # 期間の表材の作成
-            string11 = f'{start_date} から{end_date}'
-            cv.drawString(100, 570, f'{string11}納入分 請求金額:￥{int(dd_請求金額):,}円')
+        # 二列目[名称]
+        for i in range(len(df)):
+            cv.drawString(122, 514 - (i * 20), str(df.iloc[i, 1]))
 
-            cv.setLineWidth(1.5)
-            # 線を描画(始点x、始点y、終点x、終点y)
-            cv.line(95, 562, 520, 562)
-            cv.line(45, 755, 560, 755)
+        # 三列目[単価]
+        for i in range(len(df)):
+            cv.drawString(252, 514 - (i * 20), "{:.2f}".format(df.iloc[i, 2]))
 
-            # cv.setFont('HeiseiKakuGo-W5', font_size3)
-            cv.setFont('MSGothic', font_size3)
+        # 四列目[出荷数]
+        for i in range(len(df)):
+            cv.drawString(307, 514 - (i * 20), "{:,}".format(df.iloc[i, 3]))
 
-            # ヘッター欄
-            cv.drawString(52, 534, '品  番')
-            cv.drawString(132, 534, '名  称')
-            cv.drawString(262, 534, '単  価')
-            cv.drawString(312, 534, '出荷数')
-            cv.drawString(367, 534, '出荷日')
-            cv.drawString(447, 534, 'Tax')
-            cv.drawString(492, 534, '出荷金額')
+        # 五列目[出荷日]
+        for i in range(len(df)):
+            cv.drawString(357, 514 - (i * 20), str(df.iloc[i, 4]))
 
-            # 一列目[品番]
-            for i in range(len(df)):
-                cv.drawString(42, 514 - (i * 20), str(df.iloc[i, 0]))
+        # 六列目[Tax]
+        for i in range(len(df)):
+            cv.drawString(447, 514 - (i * 20), "{:,}".format(df.iloc[i, 5]))
 
-            # 二列目[名称]
-            for i in range(len(df)):
-                cv.drawString(122, 514 - (i * 20), str(df.iloc[i, 1]))
+        # 七列目[出荷金額]
+        for i in range(len(df)):
+            cv.drawString(477, 514 - (i * 20), "{:,}".format(df.iloc[i, 6]))
 
-            # 三列目[単価]
-            for i in range(len(df)):
-                cv.drawString(252, 514 - (i * 20), "{:.2f}".format(df.iloc[i, 2]))
+        # 表の作成[6列21行] # cv.showPage()  改行ページ
+        cv.setLineWidth(1)
+        xlist = (40, 120, 250, 300, 350, 440, 470, 575)  # 差分　80,160,80,80,100       6列21行
+        ylist = (
+            130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390, 410, 430, 450, 470, 490,
+            510, 530, 550)  # 差分　20(21行)
+        cv.grid(xlist, ylist)
 
-            # 四列目[出荷数]
-            for i in range(len(df)):
-                cv.drawString(307, 514 - (i * 20), "{:,}".format(df.iloc[i, 3]))
+        # 小計額/消費税/合計額の表題
+        cv.drawString(370, 104, '小計額')
+        cv.drawString(340, 85, '消費税(10%)')
+        cv.drawString(340, 65, '消費税( 8%)')
+        cv.drawString(370, 45, '合計額')
 
-            # 五列目[出荷日]
-            for i in range(len(df)):
-                cv.drawString(357, 514 - (i * 20), str(df.iloc[i, 4]))
+        # 小計額/消費税/合計額のData
+        cv.drawString(460, 105, f'￥{int(dd_出荷合計):,}円')
+        cv.drawString(460, 85, f'￥{int(dd_消費税額):,}円')
+        cv.drawString(460, 65, f'￥0円')
+        cv.drawString(460, 45, f'￥{int(dd_請求金額):,}円')
 
-            # 六列目[Tax]
-            for i in range(len(df)):
-                cv.drawString(447, 514 - (i * 20), "{:,}".format(df.iloc[i, 5]))
+        # 表の作成[1列4行]
+        cv.setLineWidth(1)
+        xlist = (410, 560)
+        ylist = (40, 60, 80, 100, 120)
+        cv.grid(xlist, ylist)
 
-            # 七列目[出荷金額]
-            for i in range(len(df)):
-                cv.drawString(477, 514 - (i * 20), "{:,}".format(df.iloc[i, 6]))
+        # Canvasを保存してファイルを作成
+        cv.save()
 
-            # 表の作成[6列21行] # cv.showPage()  改行ページ
-            cv.setLineWidth(1)
-            xlist = (40, 120, 250, 300, 350, 440, 470, 575)  # 差分　80,160,80,80,100       6列21行
-            ylist = (
-                130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390, 410, 430, 450, 470, 490,
-                510, 530, 550)  # 差分　20(21行)
-            cv.grid(xlist, ylist)
+# 保存先のディレクトリパス
+output_directory = "C:\\Users\\marom\\Invoice"
 
-            # 小計額/消費税/合計額の表題
-            cv.drawString(370, 104, '小計額')
-            cv.drawString(340, 85, '消費税(10%)')
-            cv.drawString(340, 65, '消費税( 8%)')
-            cv.drawString(370, 45, '合計額')
+# PDFを作成 # 日付をYYYYMMDD形式に変換し、ファイル名に使用する
+today = date.today()
+today_str_cnv = today.strftime('%Y%m%d')
+file_name = f'Invoice_{today_str_cnv}.pdf'
 
-            # 小計額/消費税/合計額のData
-            cv.drawString(460, 105, f'￥{int(dd_出荷合計):,}円')
-            cv.drawString(460, 85, f'￥{int(dd_消費税額):,}円')
-            cv.drawString(460, 65, f'￥0円')
-            cv.drawString(460, 45, f'￥{int(dd_請求金額):,}円')
+# ファイルのフルパス
+file_path = os.path.join(output_directory, file_name)
 
-            # 表の作成[1列4行]
-            cv.setLineWidth(1)
-            xlist = (410, 560)
-            ylist = (40, 60, 80, 100, 120)
-            cv.grid(xlist, ylist)
+# PDFを作成して保存
+create_pdf(file_path)
 
-            # PDFファイルを保存する
-            cv.save()
-
-        # 保存先のディレクトリパス
-        output_directory = "C:\\Users\\Invoice"
-
-        # 保存するファイル名
-        file_name = "invoice.pdf"
-
-        # ファイルのフルパス
-        file_path = os.path.join(output_directory, file_name)
-
-        # PDFを作成して保存
-        create_pdf(file_path)
-
-        # output_directory = "C:\\Users\\Invoice"
-        # # 保存するファイル名
-        # file_name = "invoice.pdf"
-        # # ファイルのフルパス
-        # file_path = os.path.join(output_directory, file_name)
-        # # PDFを作成して保存
-        # create_pdf(file_path)
-        #
-        # # Streamlitにファイルの保存を通知する
-        # st.write(file_name)
-        # # st.write(f"PDF file saved at:file_name")
-
-
-
-show_invoice43()
 
 # streamlit run show_invoice.py
